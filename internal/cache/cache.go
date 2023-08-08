1879317b6e6771bf2c29365939fa6332da06fd32
serhii <sergeyplakhonin@gmail.com>
2023-08-08 15:01:36 +0200

    Add TTL

--- internal/cache/cache.go
+++ internal/cache/cache.go
@@ -1,39 +1,130 @@
 package cache
 
-import "errors"
+import (
+	"errors"
+	"sync"
+	"time"
+)
+
+const (
+	cleanInterval = 5 * time.Second
+	defaultTTL    = 5 * time.Second // Default TTL
+)
 
 type Cache struct {
-	storage map[string]interface{}
+	Storage  map[string]cachedData
+	mu       *sync.RWMutex
+	cleaner  *time.Ticker
+	stopChan chan struct{}
 }
 
-// NewCache initialize a new cache instance
+type cachedData struct {
+	value  interface{}
+	expiry time.Time
+}
+
+// NewCache initializes a new cache instance with a background cleaner
 func NewCache() *Cache {
-	return &Cache{
-		storage: make(map[string]interface{}),
+	c := &Cache{
+		Storage:  make(map[string]cachedData),
+		mu:       &sync.RWMutex{},
+		cleaner:  time.NewTicker(cleanInterval),
+		stopChan: make(chan struct{}),
+	}
+
+	go c.startCleaner()
+
+	return c
+}
+
+// Clear a value from cache
+func (c *Cache) startCleaner() {
+	for {
+		select {
+		case <-c.cleaner.C:
+			c.clean()
+		case <-c.stopChan:
+			c.cleaner.Stop()
+			return
+		}
 	}
 }
 
-// Set a new value to cache
-func (c *Cache) Set(key string, value interface{}) {
-	c.storage[key] = value
+func (c *Cache) clean() {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+
+	now := time.Now()
+
+	for key, cacheData := range c.Storage {
+		if cacheData.expiry.Before(now) {
+			delete(c.Storage, key)
+		}
+	}
+}
+
+// Set a new value to cache with an optional TTL
+func (c *Cache) Set(key string, value interface{}, ttl ...time.Duration) {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+
+	var expiry time.Time
+
+	// If a TTL is provided, use it; otherwise, use the default
+	if len(ttl) > 0 {
+		expiry = time.Now().Add(ttl[0])
+	} else {
+		expiry = time.Now().Add(defaultTTL) // Use default TTL from the constant
+	}
+
+	c.Storage[key] = cachedData{
+		value:  value,
+		expiry: expiry,
+	}
 }
 
 // Get a value from cache
 func (c *Cache) Get(key string) (interface{}, error) {
-	val, ok := c.storage[key]
+	c.mu.RLock()
+	defer c.mu.RUnlock()
+
+	cacheData, ok := c.Storage[key]
 	if !ok {
-		return nil, errors.New("can not get a key, it does not exist")
+		return nil, errors.New("key does not exist")
 	}
 
-	return val, nil
+	return cacheData.value, nil
 }
 
 // Delete a value from cache
 func (c *Cache) Delete(key string) error {
-	_, ok := c.storage[key]
+	c.mu.Lock()
+	defer c.mu.Unlock()
+
+	_, ok := c.Storage[key]
 	if !ok {
-		return errors.New("can not delete by key, it does not exist")
+		return errors.New("key does not exist")
 	}
-	delete(c.storage, key)
+
+	delete(c.Storage, key)
+	return nil
+}
+
+// StopCleaner stop the background cleaner
+func (c *Cache) StopCleaner() {
+	close(c.stopChan)
+}
+
+// CacheClean removes a value from cache by key
+func (c *Cache) CacheClean(key string) error {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+
+	_, ok := c.Storage[key]
+	if !ok {
+		return errors.New("key does not exist")
+	}
+
+	delete(c.Storage, key)
 	return nil
 }
---
+++ internal/cache/cache_test.go
@@ -0,0 +1,176 @@
+package cache
+
+import (
+	"strconv"
+	"testing"
+	"time"
+)
+
+func TestCache_SetAndGet(t *testing.T) {
+	cache := NewCache()
+
+	value := "test value"
+	key := "test-key"
+
+	cache.Set(key, value)
+
+	cachedValue, err := cache.Get(key)
+	if err != nil {
+		t.Errorf("Expected no error, but got: %v", err)
+	}
+
+	if cachedValue != value {
+		t.Errorf("Expected value '%s', but got '%v'", value, cachedValue)
+	}
+}
+
+func TestCache_SetWithTTL(t *testing.T) {
+	cache := NewCache()
+
+	value := "test value"
+	key := "test-key"
+	ttl := 2 * time.Second
+
+	cache.Set(key, value, ttl)
+
+	cachedValue, err := cache.Get(key)
+	if err != nil {
+		t.Errorf("Expected no error, but got: %v", err)
+	}
+
+	if cachedValue != value {
+		t.Errorf("Expected value '%s', but got '%v'", value, cachedValue)
+	}
+}
+
+func TestCache_GetWithTTL_Success(t *testing.T) {
+	cache := NewCache()
+
+	value := "test value"
+	key := "test-key"
+	ttl := 2 * time.Second
+
+	cache.Set(key, value, ttl)
+
+	// Sleep for slightly longer than TTL to ensure expiration
+	time.Sleep(ttl + 1*time.Second)
+
+	_, err := cache.Get(key)
+	if err != nil {
+		t.Errorf("Unexpected error: %v", err)
+	}
+}
+
+func TestCache_GetWithTTL_Failed(t *testing.T) {
+	cache := NewCache()
+
+	value := "test value"
+	key := "test-key"
+	ttl := 2 * time.Second
+
+	cache.Set(key, value, ttl)
+
+	// Sleep for slightly longer than TTL to ensure expiration
+	time.Sleep(ttl + 4*time.Second)
+
+	_, err := cache.Get(key)
+	if err != nil {
+		// No assertion needed, test passes if an error is present
+		return
+	}
+
+	t.Error("Expected an error due to TTL expiration, but got NO error")
+}
+
+func TestCache_Delete(t *testing.T) {
+	cache := NewCache()
+
+	value := "test value"
+	key := "test-key"
+
+	cache.Set(key, value)
+
+	err := cache.Delete(key)
+	if err != nil {
+		t.Errorf("Expected no error, but got: %v", err)
+	}
+
+	_, err = cache.Get(key)
+	if err == nil {
+		t.Errorf("Expected an error after deleting, but got no error")
+	}
+}
+
+func TestCache_CacheClean(t *testing.T) {
+	cache := NewCache()
+
+	value := "test value"
+	key := "test-key"
+
+	cache.Set(key, value)
+
+	err := cache.CacheClean(key)
+	if err != nil {
+		t.Errorf("Expected no error, but got: %v", err)
+	}
+
+	_, err = cache.Get(key)
+	if err == nil {
+		t.Errorf("Expected an error after cleaning cache, but got no error")
+	}
+}
+
+func TestCache_StopCleaner(t *testing.T) {
+	cache := NewCache()
+
+	// Add some values to cache
+	cache.Set("key1", "value1")
+	cache.Set("key2", "value2")
+
+	// Wait for a moment to ensure the cleaner has a chance to run
+	time.Sleep(1 * time.Second)
+
+	// Stop the cleaner
+	cache.StopCleaner()
+
+	// Wait for a moment to ensure the cleaner is fully stopped
+	time.Sleep(1 * time.Second)
+
+	// Try to clean cache manually (which shouldn't work after stopping the cleaner)
+	cache.clean()
+
+	// Ensure that the cache is not cleaned and still contains the old values
+	_, err := cache.Get("key1")
+	if err != nil {
+		t.Errorf("Expected no error, but got: %v", err)
+	}
+
+	_, err = cache.Get("key2")
+	if err != nil {
+		t.Errorf("Expected no error, but got: %v", err)
+	}
+}
+
+func TestCache_ConcurrentAccess(t *testing.T) {
+	cache := NewCache()
+
+	// Concurrently set and get values
+	const numRoutines = 10
+	for i := 0; i < numRoutines; i++ {
+		go func(idx int) {
+			key := "key" + strconv.Itoa(idx)
+			value := "value" + strconv.Itoa(idx)
+			cache.Set(key, value)
+			cachedValue, err := cache.Get(key)
+			if err != nil {
+				t.Errorf("Routine %d: Expected no error, but got: %v", idx, err)
+			}
+			if cachedValue != value {
+				t.Errorf("Routine %d: Expected value '%s', but got '%v'", idx, value, cachedValue)
+			}
+		}(i)
+	}
+
+	// Wait for routines to complete
+	time.Sleep(2 * time.Second)
+}
